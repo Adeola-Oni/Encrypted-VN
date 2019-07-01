0641725be764f7981c29d9c9ea3a352011dc8744
Zaida-3dO <opsijay@gmail.com>
2019-07-01 13:21:39 +0100

    File encrypter lib

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View , Image, } from 'react-native';
 import image1 from './images/img1.svg';
-import './App.css';
+// import './App.css';
 import startIcon from './images/start.png';
 // import SlidePart from './Reciever/SlidePart';
 import TestApp from './Reciever/Stopwatch'
 import Overlay from './Reciever/Overlay'
 import { Header, PricingCard, Input,Card, Button, Icon, ButtonGroup, Slider } from 'react-native-elements';
 
-export default function App() {
+export default function App(props) {
   const buttons = ['Start', 'Stop']
   return (
     <View style={styles.container}>
@@ -29,18 +29,18 @@ export default function App() {
               <div className='pack'>
                 <Image
                     style={{width: 180, height: 180, alignItems: 'center', marginTop: '20%', marginBottom: '10%'}}
-                    source={require('./images/microphone.svg')} 
+                    source={require('./images/microphone.svg')}
                 />
                 <br/>
                 <Image
                     style={{width: 50, height: 50, alignItems: 'center', marginBottom: '30%'}}
-                    source={require('./images/pulse.svg')} 
+                    source={require('./images/pulse.svg')}
                 />
               </div>
               <div className='stopwatch'>
                 <TestApp style={styles.stopwatch}/>
               </div>
-             
+
           </div>
         </Text>
     </View>
